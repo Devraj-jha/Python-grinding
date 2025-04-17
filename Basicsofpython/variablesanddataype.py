066c3949d0720fbb9a83e0a0d4382c3e34854622
Devraj-jha <devrajjha100806@gmail.com>
2025-04-17 07:05:13 +0530

    variables

---
+++ Basicsofpython/variablesanddataype.py
@@ -0,0 +1,22 @@
+a = 10          # int
+b = 3.5         # float
+c = "hello"     # string
+d = True        # boolean
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+x = int("5")
+print(x)
+name = input("Enter your name:  ")
+print("Hello ,", name)
+
+
+name = input("Enter your name:  ")
+age = input("Enter your age:  ")
+currentyear = 2025
+year100 =  currentyear + 100 - int(age)
+
+print("Hello ", name , "you'll turn ", age, "in the year",year100)
