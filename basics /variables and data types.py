f2a1e9250d5713fed3ee5d6b8fd2912e58d2fdc8
Devraj-jha <devrajjha100806@gmail.com>
2025-06-16 19:38:53 +0530

    dev

---
+++ basics /variables and data types.py
@@ -0,0 +1,11 @@
+age = 19        # int
+pi = 3.1416     # float
+name = "DJ"     # str
+is_cool = True  # bool
+value = None    # NoneType
+
+
+print(type(4)) #type is use to check type of a data
+
+print(isinstance(3,str)) 
+#to check if a string is of spectifc data type
